240d8c572e4e6fa75b2f903da77adb10895eae39
Millington-Holi7 <holinas007@gmail.com>
2024-07-13 14:47:27 -0400

    create login and signup feature.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+# app/controllers/users_controller.rb
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = 'User created successfully!'
+      redirect_to '/'
+    else
+      flash[:alert] = @user.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,46 @@
 Rails.application.routes.draw do
-
-  namespace :admin do
-    get 'categories/index'
-    get 'categories/new'
-    get 'categories/create'
-  end
+  # Root route
   root to: 'products#index'
 
-  resources :products, only: [:index, :show]
-  resources :categories, only: [:show]
+  # Sessions routes
+  get 'sessions/new'
+  post 'sessions/create'
+  delete 'sessions/destroy'
 
-  resource :cart, only: [:show] do
-    post   :add_item
-    post   :remove_item
-  end
+  # Users routes
+  get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'
+  get 'users/new', to: 'users#new'
 
-  resources :orders, only: [:create, :show]
 
+
+  # Admin namespace routes
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
   end
 
-# about page route
- get 'about', to: 'about#index', as: 'about'
- 
+  # Resources routes
+  resources :products, only: [:index, :show]
+  resources :categories, only: [:show]
+
+  resource :cart, only: [:show] do
+    post :add_item
+    post :remove_item
+  end
+
+  resources :orders, only: [:create, :show]
+
+  # About page route
+  get 'about', to: 'about#index', as: 'about'
+
+  # User registration, login, and logout
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
